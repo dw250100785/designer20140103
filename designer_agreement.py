96a92f18758a426865b9c25f75de569602239fed
dengwei <250100785@qq.com>
2013-11-09 18:05:28 +0800

    多用户多角色权限分布测试

--- designer_agreement.py
+++ designer_agreement.py
@@ -23,6 +23,7 @@ import openerp.addons.decimal_precision as dp
 from openerp.osv import osv
 from openerp.osv import fields
 from openerp.tools.translate import _
+import math
 import time
 class designer_contract_type(osv.osv):
     """ 品牌"""
@@ -41,11 +42,50 @@ class designer_agreement(osv.osv):
     """ 扩展发票管理"""
     _name = "designer.agreement"
     _inherit = ['mail.thread']
+
+
+
+    #人民币金额转大写程序Python版本
+    #Copyright: zinges at foxmail.com
+    #blog: http://zingers.iteye.com
+    #感谢zinges提供了Python的版本
+
+    def numtoCny(num):
+        capUnit = ['万','亿','万','圆','']
+        capDigit = { 2:['角','分',''], 4:['仟','佰','拾','']}
+        capNum=['零','壹','贰','叁','肆','伍','陆','柒','捌','玖']
+        snum = str('%019.02f') % num
+        if snum.index('.')>16:
+            return ''
+        ret,nodeNum,subret,subChr='','','',''
+        CurChr=['','']
+        for i in range(5):
+            j=int(i*4+math.floor(i/4))
+            subret=''
+            nodeNum=snum[j:j+4]
+            lens=len(nodeNum)
+            for k in range(lens):
+                if int(nodeNum[k:])==0:
+                    continue
+                CurChr[k%2] = capNum[int(nodeNum[k:k+1])]
+                if nodeNum[k:k+1] != '0':
+                    CurChr[k%2] += capDigit[lens][k]
+                if  not ((CurChr[0]==CurChr[1]) and (CurChr[0]==capNum[0])):
+                    if not((CurChr[k%2] == capNum[0]) and (subret=='') and (ret=='')):
+                        subret += CurChr[k%2]
+            subChr = [subret,subret+capUnit[i]][subret!='']
+            if not ((subChr == capNum[0]) and (ret=='')):
+                ret += subChr
+        return [ret,capNum[0]+capUnit[3]][ret=='']
+
+    def onchange_contractamount(self, cr, uid, ids, contract_amount, context=None):
+        return {'value':{'contract_amount_big': self.numtoCny(contract_amount) }}
+
     _columns = {
         'partner_id':fields.many2one('res.partner', '客户', required=True,
             change_default=True, track_visibility='always'),
         'contract_type': fields.many2one('designer.contract.type',string='合同类型', required=True),
-        'contract_amount': fields.float('合同金额', digits_compute=dp.get_precision('contract_amount'),required=True),
+        'contract_amount': fields.float('合同金额', on_change="onchange_contractamount(contract_amount)", digits_compute=dp.get_precision('contract_amount'),required=True),
         'contract_amount_big': fields.char('合同金额大写', required=True),
         'project_ids': fields.many2one('designer.project', string='项目简报'),
         'card_line': fields.one2many('designer.agreement.rule.line', 'card_id', '付款方式'),
